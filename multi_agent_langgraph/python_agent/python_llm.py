50351c0fab263fdb8456e101d716ce3fd71d5535
Arka Patra <arkapatra2009@gmail.com>
2025-03-01 01:23:41 +0530

    Add initial project files and dependencies for Python LLM application
    Signed-off-by: Arka Patra <arkapatra2009@gmail.com>

---
+++ multi_agent_langgraph/python_agent/python_llm.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain_core.prompts import ChatPromptTemplate
+
+load_dotenv()
+
+prompt_template = ChatPromptTemplate.from_template(
+    template= """ You will be provided a Python question. Return only the code to solve the problem. Here is the question: {question}"""
+)
+
+llm = ChatOpenAI(
+    model='gpt-4o-mini',
+    temperature=0,
+    verbose= True
+)
+
+chain = prompt_template | llm
+
+response = chain.invoke(
+    {
+        "question": "**Question:** Write a Python function that takes a list of numbers and returns the sum of all the even numbers in the list."
+    }
+)
+
+print(response.content)
+
