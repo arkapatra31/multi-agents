e1b4b43ac8151c3677003f440a6cd1d477d946e7
Arka Patra <arkapatra@deloitte.com>
2025-07-14 21:13:03 +0530

    Remove flight and hotel agents along with their associated tools and orchestrator to streamline the multi-agent swarm architecture.

--- multi_agent_swarm/flight_search_agent/__init__.py
+++
@@ -1,3 +0,0 @@
-from multi_agent_swarm.flight_search_agent.flight_agent import run_flight_agent
-
-__all__ = [run_flight_agent]
--- multi_agent_swarm/swarm/__init__.py
+++
@@ -1,3 +0,0 @@
-from multi_agent_swarm.swarm.orchestrator import run_orchestrator
-
-__all__ = [run_orchestrator]
